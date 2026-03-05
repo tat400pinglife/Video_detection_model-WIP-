198760c46c90533b40b64fb1e1d5687359cc0313
TehseenChowdhury <124292037+TehseenChowdhury@users.noreply.github.com>
2026-03-05 01:53:49 +0000

    Set up for tmux session

--- generate_tensors_gpu_launcher.py
+++ generate_tensors_gpu_launcher.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         path=Path("./data/videos/temp"),
         start=0,
         goal=3,                  # big number
-        wait=5,                       # wait 3 seconds to avoid ban.
+        wait=15,                       # wait 3 seconds to avoid ban.
         fn=process_video_gpu,         # GPU please work
         delete_after=True,            # Deletes the mp4 after processing
         output_dir="./data/processed_data/fake",
